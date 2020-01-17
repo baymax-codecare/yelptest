b873215bf76bf669757d04e5524611442c83ffb2
full developer <fulldeveloper1215@gmail.com>
2020-01-17 23:49:42 +0700

    Initial dist subtree commit

---
+++ dist/precache-manifest.c98181638d2bbf8aa1fc600029b75854.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27e2bb5b90e783df68c",
+    "url": "yelptest/css/app.24f1c57e.css"
+  },
+  {
+    "revision": "b4bf1bd9a18c251da81d02139f2bc6d6",
+    "url": "yelptest/css/landing_picture.css"
+  },
+  {
+    "revision": "699d74fa42a955783cb0cd58be2db779",
+    "url": "yelptest/css/yp-chunck.css"
+  },
+  {
+    "revision": "5e195265495563fe2465ff989bafd5c1",
+    "url": "yelptest/css/yp-original.css"
+  },
+  {
+    "revision": "118ff475a341620f50dfbaddb83efb25",
+    "url": "yelptest/favicon-yelp.ico"
+  },
+  {
+    "revision": "002ab395a1d2c2a910894e99a4d405a8",
+    "url": "yelptest/index.html"
+  },
+  {
+    "revision": "1dbc016df7d228bf9fa1",
+    "url": "yelptest/js/about.ea0e1ed1.js"
+  },
+  {
+    "revision": "a27e2bb5b90e783df68c",
+    "url": "yelptest/js/app.fc7f1db6.js"
+  },
+  {
+    "revision": "7b3f22d231d7deeb2cf0",
+    "url": "yelptest/js/chunk-vendors.35f07cce.js"
+  },
+  {
+    "revision": "35fa0e5bdbd313ee98c482478499d7f6",
+    "url": "yelptest/js/svg_sprite.js"
+  },
+  {
+    "revision": "8b03f533011080f8ac9b537c420986a8",
+    "url": "yelptest/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "yelptest/robots.txt"
+  }
+]);
