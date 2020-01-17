e6d16c1c01df1784bf72f59eaf604b689503c2fc
full developer <fulldeveloper1215@gmail.com>
2020-01-18 01:06:25 +0700

    Initial dist subtree commit

--- precache-manifest.b53c1caf918cddfd57c629b09aa7e7ac.js
+++ precache-manifest.b53c1caf918cddfd57c629b09aa7e7ac.js
@@ -1,50 +1,50 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6f3409e1225656f0838e",
-    "url": "/css/app.8ef6bbe0.css"
+    "revision": "048d5822d5a8dfa947a5",
+    "url": "yelptest/css/app.8ef6bbe0.css"
   },
   {
     "revision": "b4bf1bd9a18c251da81d02139f2bc6d6",
-    "url": "/css/landing_picture.css"
+    "url": "yelptest/css/landing_picture.css"
   },
   {
     "revision": "699d74fa42a955783cb0cd58be2db779",
-    "url": "/css/yp-chunck.css"
+    "url": "yelptest/css/yp-chunck.css"
   },
   {
     "revision": "5e195265495563fe2465ff989bafd5c1",
-    "url": "/css/yp-original.css"
+    "url": "yelptest/css/yp-original.css"
   },
   {
     "revision": "118ff475a341620f50dfbaddb83efb25",
-    "url": "/favicon-yelp.ico"
+    "url": "yelptest/favicon-yelp.ico"
   },
   {
-    "revision": "b20ddb83c8127d874f0c43956aec3c9d",
-    "url": "/index.html"
+    "revision": "6ed57a797066f1d2eef2fcc3f9fffc2c",
+    "url": "yelptest/index.html"
   },
   {
     "revision": "1dbc016df7d228bf9fa1",
-    "url": "/js/about.ea0e1ed1.js"
+    "url": "yelptest/js/about.ea0e1ed1.js"
   },
   {
-    "revision": "6f3409e1225656f0838e",
-    "url": "/js/app.e99ea94e.js"
+    "revision": "048d5822d5a8dfa947a5",
+    "url": "yelptest/js/app.41453b09.js"
   },
   {
     "revision": "7b3f22d231d7deeb2cf0",
-    "url": "/js/chunk-vendors.35f07cce.js"
+    "url": "yelptest/js/chunk-vendors.35f07cce.js"
   },
   {
     "revision": "35fa0e5bdbd313ee98c482478499d7f6",
-    "url": "/js/svg_sprite.js"
+    "url": "yelptest/js/svg_sprite.js"
   },
   {
     "revision": "8b03f533011080f8ac9b537c420986a8",
-    "url": "/manifest.json"
+    "url": "yelptest/manifest.json"
   },
   {
     "revision": "735ab4f94fbcd57074377afca324c813",
-    "url": "/robots.txt"
+    "url": "yelptest/robots.txt"
   }
 ]);
